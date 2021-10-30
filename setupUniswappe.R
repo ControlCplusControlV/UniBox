269744d82cf68ee1add2a61ec31df64d26a810dd
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-10-30 13:23:06 -0700

    Add files via upload

---
+++ setupUniswappe.R
@@ -0,0 +1,8 @@
+#!/usr/bin/Rscript
+
+install.packages("devtools")
+install.packages("Rserve")
+devtools::install_github("Omni-Analytics-Group/uniswappeR")
+library(reticulate)
+require('Rserve')
+run.Rserve()
