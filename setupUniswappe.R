72a4b5f006777db400e1335c5ff76d7295340545
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-10-30 21:18:07 -0600

    Delete setupUniswappe.R

--- setupUniswappe.R
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/Rscript
-
-install.packages("devtools")
-install.packages("Rserve")
-devtools::install_github("Omni-Analytics-Group/uniswappeR")
-library(reticulate)
-require('Rserve')
-run.Rserve()
