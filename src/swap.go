84047a313917e7189affec7898f9a10e6f5efe20
DZuko <djcouser@gmail.com>
2021-11-02 19:23:16 -0400

    Removed block number from contract.Txn. Changed naming convention for uniswapRouter instance

--- src/swap.go
+++ src/swap.go
@@ -13,7 +13,7 @@ import (
 //Web3 HTTP client to send calls to the RPC enpoint
 var web3Client = initWeb3Client()
 
-var uniswapRouterV2 = initUniswapRouterV2()
+var uniswapRouter02 = initUniswapRouter02()
 
 //initialize web3 http client
 func initWeb3Client() *jsonrpc.Client {
@@ -27,62 +27,62 @@ func initWeb3Client() *jsonrpc.Client {
 }
 
 //initialize uniswap router v2 contract instance
-func initUniswapRouterV2() *contract.Contract {
+func initUniswapRouter02() *contract.Contract {
 	//initialize a web3 address with the uniswap router hex address
-	uniswapRouterV2Address := web3.HexToAddress("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
+	uniswapRouter02Address := web3.HexToAddress("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
 	//TODO: need to read in abi from file
-	//read in the uniswapRouterV2 abi
+	//read in the uniswapRouter02 abi
 	abi, err := abi.NewABI("abi from file")
 	if err != nil {
 		fmt.Println("Error when reading Uniswap ABI", err)
 		return nil
 	}
-	return contract.NewContract(uniswapRouterV2Address, abi, web3Client)
+	return contract.NewContract(uniswapRouter02Address, abi, web3Client)
 }
 
 //Uniswap Router02 swap functions--------------------------
 
 func SwapExactTokensForTokens(amountIn uint, amountOutMin uint, path []common.Address, to common.Address, deadline uint) *contract.Txn {
-	txn := uniswapRouterV2.Txn("swapExactTokensForTokens", web3.Latest, amountIn, amountOutMin, path, to, deadline)
+	txn := uniswapRouter02.Txn("swapExactTokensForTokens", web3.Latest, amountIn, amountOutMin, path, to, deadline)
 	return txn
 }
 
 func SwapTokensForExactTokens(amountOut uint, amountInMax uint, path []common.Address, to common.Address, deadline uint) *contract.Txn {
-	txn := uniswapRouterV2.Txn("swapTokensForExactTokens", web3.Latest, amountOut, amountInMax, path, to, deadline)
+	txn := uniswapRouter02.Txn("swapTokensForExactTokens", amountOut, amountInMax, path, to, deadline)
 	return txn
 }
 
 func SwapExactETHForTokens(amountOutMin uint, path []common.Address, to common.Address, deadline uint) *contract.Txn {
-	txn := uniswapRouterV2.Txn("swapExactETHForTokens", web3.Latest, amountOutMin, path, to, deadline)
+	txn := uniswapRouter02.Txn("swapExactETHForTokens", amountOutMin, path, to, deadline)
 	return txn
 }
 
 func SwapTokensForExactETH(amountOut uint, amountInMax uint, path []common.Address, to common.Address, deadline uint) *contract.Txn {
-	txn := uniswapRouterV2.Txn("swapTokensForExactETH", web3.Latest, amountOut, amountInMax, path, to, deadline)
+	txn := uniswapRouter02.Txn("swapTokensForExactETH", amountOut, amountInMax, path, to, deadline)
 	return txn
 }
 
 func SwapExactTokensForETH(amountIn uint, amountOutMin uint, path []common.Address, to common.Address, deadline uint) *contract.Txn {
-	txn := uniswapRouterV2.Txn("swapExactTokensForETH", web3.Latest, amountIn, amountOutMin, path, to, deadline)
+	txn := uniswapRouter02.Txn("swapExactTokensForETH", amountIn, amountOutMin, path, to, deadline)
 	return txn
 }
 
 func SwapETHForExactTokens(amountOut uint, path []common.Address, to common.Address, deadline uint) *contract.Txn {
-	txn := uniswapRouterV2.Txn("swapETHForExactTokens", web3.Latest, amountOut, path, to, deadline)
+	txn := uniswapRouter02.Txn("swapETHForExactTokens", amountOut, path, to, deadline)
 	return txn
 }
 
 func SwapExactTokensForTokensSupportingFeeOnTransferTokens(amountIn uint, amountOutMin uint, path []common.Address, to common.Address, deadline uint) *contract.Txn {
-	txn := uniswapRouterV2.Txn("swapExactTokensForTokensSupportingFeeOnTransferTokens", web3.Latest, amountIn, amountOutMin, path, to, deadline)
+	txn := uniswapRouter02.Txn("swapExactTokensForTokensSupportingFeeOnTransferTokens", amountIn, amountOutMin, path, to, deadline)
 	return txn
 }
 
 func SwapExactETHForTokensSupportingFeeOnTransferTokens(amountOutMin uint, path []common.Address, to common.Address, deadline uint) *contract.Txn {
-	txn := uniswapRouterV2.Txn("swapExactETHForTokensSupportingFeeOnTransferTokens", web3.Latest, amountOutMin, path, to, deadline)
+	txn := uniswapRouter02.Txn("swapExactETHForTokensSupportingFeeOnTransferTokens", amountOutMin, path, to, deadline)
 	return txn
 }
 
 func SwapExactTokensForETHSupportingFeeOnTransferTokens(amountIn uint, amountOutMin uint, path []common.Address, to common.Address, deadline uint) *contract.Txn {
-	txn := uniswapRouterV2.Txn("swapExactTokensForETHSupportingFeeOnTransferTokens", web3.Latest, amountIn, amountOutMin, path, to, deadline)
+	txn := uniswapRouter02.Txn("swapExactTokensForETHSupportingFeeOnTransferTokens", amountIn, amountOutMin, path, to, deadline)
 	return txn
 }
